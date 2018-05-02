39668acf554620637ae87667bdab2ed6080011ed
Faisal Alam <faisal.11616526@gmail.com>
2018-05-02 14:34:04 +0530

    Fixed issues.

--- CSE325/Practice/transfer_file2file.c
+++ CSE325/Practice/transfer_file2file.c
@@ -5,31 +5,33 @@
 #include<sys/types.h>
 
 int main() {
-    int fd, n;
+    int fd1, fd2, n;
     char file1[100], file2[100], buff[100];
 
     printf("Enter the name of file to copy content from: ");
     scanf("%s", file1);
 
-    fd = open(file1, O_RDONLY);
-    if(fd == -1) {
+    fd1 = open(file1, O_RDONLY);
+    if(fd1 == -1) {
         printf("Error opening file try again.\n");
         exit(EXIT_FAILURE);
     }
-
-    n = read(fd, buff, sizeof(buff));
-    close(fd);
 
     printf("Enter the name of file to copy content to: ");
     scanf("%s", file2);
 
-    fd = open(file2, O_CREAT|O_TRUNC|O_WRONLY);
-    if(fd == -1) {
+    fd2 = open(file2, O_CREAT|O_TRUNC|O_WRONLY, 0777);
+    if(fd2 == -1) {
         printf("Error opening file try again.\n");
         exit(EXIT_FAILURE);
     }
-    write(fd, buff, n);
-    close(fd);
+
+    while ((n = read(fd1, buff, sizeof(buff))) != 0) {
+		write(fd2, buff, n);
+	}
+
+    close(fd1);
+	close(fd2);
 
     printf("Data successfully copied from %s to %s.\n",file1, file2);
     exit(EXIT_SUCCESS);
