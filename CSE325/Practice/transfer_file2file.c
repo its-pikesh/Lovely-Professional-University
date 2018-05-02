f132683de1a3591f5849eea19b3055a426f299c6
Faisal Alam <faisal.11616526@gmail.com>
2018-05-02 14:17:00 +0530

    Truncate the to-file if already exists.

--- CSE325/Practice/transfer_file2file.c
+++ CSE325/Practice/transfer_file2file.c
@@ -4,29 +4,33 @@
 #include<fcntl.h>
 #include<sys/types.h>
 
-int main()
-{
-	int fd,n;
-	char file1[100],file2[100],buff[100];
+int main() {
+	int fd, n;
+	char file1[100], file2[100], buff[100];
+
 	printf("Enter the name of file to copy content from: ");
-	scanf("%s",file1);
-	fd=open(file1,O_RDONLY);
-	if(fd == -1)
-	{
+	scanf("%s", file1);
+
+	fd = open(file1, O_RDONLY);
+	if(fd == -1) {
 		printf("Error opening file try again.\n");
 		exit(EXIT_FAILURE);
 	}
-	n=read(fd,buff,100);
+
+	n = read(fd, buff, sizeof(buff));
 	close(fd);
+
 	printf("Enter the name of file to copy content to: ");
-	scanf("%s",file2);
-	fd=open(file2,O_CREAT|O_WRONLY);
-	if(fd == -1)
-	{
+	scanf("%s", file2);
+
+	fd = open(file2, O_CREAT|O_TRUNC|O_WRONLY);
+	if(fd == -1) {
 		printf("Error opening file try again.\n");
 		exit(EXIT_FAILURE);
 	}
-	write(fd,buff,n);
+	write(fd, buff, n);
 	close(fd);
-	printf("Data successfully copied from %s to %s\n",file1,file2);
+
+	printf("Data successfully copied from %s to %s.\n",file1, file2);
+    exit(EXIT_SUCCESS);
 }
